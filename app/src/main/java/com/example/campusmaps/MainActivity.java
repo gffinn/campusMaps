5a46a5efcf229bcb395f81f87e0c5b22f723d8e6
Grant Finn <finng@umich.edu>
2022-11-09 19:12:25 -0500

    added registration page

--- app/src/main/java/com/example/campusmaps/MainActivity.java
+++ app/src/main/java/com/example/campusmaps/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.campusmaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -20,22 +21,41 @@ public class MainActivity extends AppCompatActivity {
         TextView password = (TextView) findViewById(R.id.password);
 
         MaterialButton loginButton = (MaterialButton) findViewById(R.id.loginButton);
+        TextView NewAcct = (TextView)findViewById(R.id.NewAcct);
 
         //Set username and password as:
         //username: admin
         //password: admin
+
+        NewAcct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    opensignupPage();
+
+            }
+        });
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
                     //correct
-                    //CHANGE THIS to the next page ****************************************************************
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                    openActivity2();
                 else
                     Toast.makeText(MainActivity.this,"LOGIN FAILED", Toast.LENGTH_SHORT).show();
             }
         });
 
+    }
+    //This opens the Next screen
+    public void openActivity2(){
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+    //This opens the Next screen
+    public void opensignupPage(){
+        Intent intent = new Intent(this, signupPage.class);
+        startActivity(intent);
     }
 }
